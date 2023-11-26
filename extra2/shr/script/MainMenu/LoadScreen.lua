3e5bb8a522bfbaff4983af337b70c03fde441201
Pat97XD <p_siedler_97@gmx.de>
2023-11-26 12:28:47 +0100

    Deactivated funfacts

--- extra2/shr/script/MainMenu/LoadScreen.lua
+++ extra2/shr/script/MainMenu/LoadScreen.lua
@@ -19,16 +19,20 @@ function LoadScreen_Init( _LoadSaveGameFlag, _MapName, _MapType, _MapCampaignNam
 	if _LoadSaveGameFlag == 0 then
 		-- Nope: get map name and description
 		local MapNameString, MapDescString = Framework.GetMapNameAndDescription( _MapName, _MapType, _MapCampaignName )
+		XGUIEng.ShowWidget( "LoadScreen_RandomInfo", 1 )
 		XGUIEng.SetText( "LoadScreen_MapInfo", MapNameString )
 		XGUIEng.SetText( "LoadScreen_RandomInfo", MapDescString )
 	else
 		-- Yes: get save game name and random info
 		local Description = Framework.GetSaveGameString( _MapName )
 		XGUIEng.SetText( "LoadScreen_MapInfo", Description )
-
+		
+		XGUIEng.ShowWidget( "LoadScreen_RandomInfo", 0 )
+		--[[
 		local FunFactNumber = 1 + XGUIEng.GetRandom( 18 )
 		local FunFactString = XGUIEng.GetStringTableText("LoadScreenTipps/LoadScreen_Header") .. ": @cr @cr "
 		XGUIEng.SetText("LoadScreen_RandomInfo", FunFactString .. XGUIEng.GetStringTableText("LoadScreenTipps/LoadScreen_Tipp" .. FunFactNumber))
+		--]]
 	end
 
 	-- Set dev news
